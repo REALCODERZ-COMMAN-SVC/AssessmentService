eebb747ee069dece05248c46dc5be96e9fca0c41
ansarianwar <md.anwar@realcoderz.in>
2022-04-13 13:07:32 +0530

    some issues

---
+++ src/main/java/com/realcoderz/assessmentservice/config/SwaggerConfig.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.realcoderz.assessmentservice.config;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.xmlpull.v1.XmlPullParserException;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author anwar
+ */
+@Profile("!prod")
+@Component
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    @Bean
+    public Docket api() throws IOException, XmlPullParserException {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.realcoderz.assessmentservice.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder().title("Assessment Service")
+                        .description(" Documentation Generateed Using SWAGGER2 for our Rest API")
+                        .version("2.0").build());
+    }
+}
