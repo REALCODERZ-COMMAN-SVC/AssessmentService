b816fe2a3452bd76c23fc07d944029b34a2bd3da
ansarianwar <md.anwar@realcoderz.in>
2022-04-13 12:05:58 +0530

    some issues

--- src/main/java/com/realcoderz/assessmentservice/config/WebMvcConfig.java
+++ src/main/java/com/realcoderz/assessmentservice/config/WebMvcConfig.java
@@ -11,9 +11,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-            .maxAge(MAX_AGE_SECS);
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
